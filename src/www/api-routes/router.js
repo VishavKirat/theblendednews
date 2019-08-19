af91b5fc2b02699d700809a621d783f6dcc2e753
Vishavkirat <vishavkiratsingh@gmail.com>
2019-08-19 17:00:25 -0400

    breaking commit...

--- src/www/api-routes/router.js
+++ src/www/api-routes/router.js
@@ -1,11 +1,23 @@
 import express from 'express';
+import axios from 'axios';
+import path from 'path';
+import dotenv from 'dotenv';
 
 // initialize Express Routers
 const router = express.Router();
+const url = 'https://newsapi.org/v2';
+dotenv.config(path.resolve(__dirname, '../../../../variable.env'));
 
-router.get('/', (req, res) => {
+router.get('/everything', (req, res) => {
   res.set('Content-Type', 'text/json');
-  res.json({ success: 'you have reached the endpoint' });
+  // `${url}/everything/${process.env.NEWS_API_KEY}`
+  Promise.resolve(
+    axios
+      .get(`${url}/everything?q=name&apiKey=${process.env.NEWS_API_KEY}`)
+      .then((payload) => res.json(payload.data)),
+  )
+    .then(() => console.log('done!!'))
+    .catch((err) => console.log(err));
 });
 
 export default router;
