5c1836cddad3c2305084fc597e328e72d098772a
Vishavkirat <vishavkiratsingh@gmail.com>
2019-08-21 00:33:14 -0400

    blended news..

---
+++ src/client/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './Components/ClientRoutes';
+
+ReactDOM.render('hello,vishav', document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,30 @@
 const path = require('path');
+
 module.exports = {
-    mode: "development",
-    entry:path.resolve(__dirname,'src/www'),
-    output: {
-        path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js',
-        publicPath:'/dist'
-    },
-    watch: true,
-    devServer: {
-        historyApiFallback: true,
-        inline: true,
-        contentBase:('src'),
-        watchContentBase: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: [/node_modules/],
-                loader: "babel-loader"
-            } 
-        ]
-    },
-    resolve: {
-        extensions: ['.js','.jsx','.es6']
-    },
-}
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/client/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/',
+  },
+  watch: true,
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    contentBase: 'src/client/public',
+    watchContentBase: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.es6'],
+  },
+};
